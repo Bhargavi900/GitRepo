4aa718b6d466f8cb5a7c1e2140d8916fc7c01001
Bhargavi <sundarapalli-sriveera.bhargavi@capgemini.com>
2024-08-14 11:27:33 +0530

    arrays to git

---
+++ Arrays.js
@@ -0,0 +1,14 @@
+//creating arrays
+let arr1=[];
+let arr2=["apple","banana"];
+let arr3=new Array();
+let arr4=new Array("apple","banna");
+
+//accessing values
+let names=["satya","sri","bhagi"];
+console.log(`Names length: ${names.length}`);
+console.log(`First Name: ${names[0]}`);
+
+//Seraching for valuews
+const includesSatya=names.includes("satya");
+console.log(`includes satya: ${includesSatya}`);
